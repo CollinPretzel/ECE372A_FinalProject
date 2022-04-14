c7fa0a23256fdf6036f48249ecadd400dfb343af
CollinPretzel <collinpreszler@email.arizona.edu>
2022-04-14 08:56:07 -0700

    Update microphone.cpp

--- src/microphone.cpp
+++ src/microphone.cpp
@@ -3,9 +3,9 @@
 #include <avr/io.h>
 
 void initMic(){
-  
+  // Pretty sure it's unnecessary, A0 is initialized in ADC code
 }
 
-void freqID(unsigned char *note, unsigned char *octave){
-  
+void freqID(unsigned char *note, unsigned char *octave, unsigned char freq){
+  // Calculate note and octave using given frequency - reference MIDI Chart
 }
